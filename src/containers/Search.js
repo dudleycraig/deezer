9c19b249ad21a2fef8b831e979d0ce3e46f62f32
Dudley <dudley@node.home>
2018-09-12 10:41:59 +0000

    added search container layout

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -2,7 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 const Search = props => {
-  return (<div className="context" id="search"></div>);
+  return (
+    <div className="context" id="search">
+      <form className="inline">
+        <input 
+          type="text" 
+          placeholder="Search albums by artist name"
+          value=""
+        />
+        <input 
+          type="submit" 
+          value="Search" 
+        />
+      </form>
+    </div>
+  );
 };
 
 const SearchContainer = connect(state => state)(Search);
